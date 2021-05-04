cd5b86c78f79311dcf816071f7042f184b7b6d70
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-05-04 17:33:17 -0400

    Actualizado script add_to_json

--- add_to_json.js
+++ add_to_json.js
@@ -4,19 +4,23 @@ const readline = require("readline-sync");
 let scp = {
     title: "",
     class: "",
+    name: "",
     description: "",
     link: ""
 }
 
 console.log("Write SCP Number: ");
 let number = readline.question();
+console.log("Write SCP name: ");
+let name = readline.question();
 console.log("Write SCP Class: ")
 let scp_class = readline.question();
-console.log("Write description: ");
+console.log("Write SCP description: ");
 let description = readline.question();
 
 scp.title = "SCP-"+number;
 scp.class = scp_class;
+scp.name = name;
 scp.description = description;
 scp.link = "http://scp-wiki.wikidot.com/scp-"+number;
 
