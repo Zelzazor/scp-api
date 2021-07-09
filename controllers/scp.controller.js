15e6371002812f4cc8f7e93556c562f09d0ce613
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-07-09 17:58:36 -0400

    Cambio a SSH

--- controllers/scp.controller.js
+++ controllers/scp.controller.js
@@ -5,11 +5,7 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-    host: process.env.DB_SERVER_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DB_URI,
     ssl: {
         rejectUnauthorized: false
       }
