cd26e29a5d727505509d7e53eca7265d05804b3c
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-07-09 16:22:23 -0400

    Actualizados endpoints para poder usar la base de datos

--- controllers/scp.controller.js
+++ controllers/scp.controller.js
@@ -1,63 +1,73 @@
 require('dotenv').config();
 const encodedKey = process.env.KEY;
 const fs = require('fs');
+const { Pool } = require('pg');
 
-const getAllSCPs = (req, res) => {
+
+const pool = new Pool({
+    host: process.env.DB_SERVER_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: true
+  });
+
+const getAllSCPs =  async (req, res) => {
     if(!req.headers.authorization){
         return res.json({"error": "Not Authorized"});
      }
      else if(Buffer.from(req.headers.authorization.split(' ')[1]).toString('base64') != encodedKey){
         return res.json({"error": "Not Authorized"});
      }
-     fs.readFile(`./scp/scp.json`, (err, data) => {
-         if (err) throw err;
-         let result = JSON.parse(data);
-         res.json(result);
-     });
+        const data = await pool.query('SELECT item_number, object_class, series, name, description, link FROM SCP ORDER BY item_number ASC');
+        let result = data.rows;
+        return res.json(result);
+     
 }
 
-const getSCP = (req, res) => {
+const getSCP = async (req, res) => {
     if(!req.headers.authorization){
         return res.json({"error": "Not Authorized"});
      }
      else if(Buffer.from(req.headers.authorization.split(' ')[1]).toString('base64') != encodedKey){
         return res.json({"error": "Not Authorized"});
      }
-    fs.readFile(`./scp/scp.json`, (err, data) => {
-        if (err) throw err;
-        let result = JSON.parse(data);
-        let filter = result.find(item => item.title === `SCP-${req.params.number}`)
-        res.json(filter);
-    });
-}
-
-const getRandomSCP = (req, res) => {
-    if(!req.headers.authorization){
-        return res.json({"error": "Not Authorized"});
-     }
-     else if(Buffer.from(req.headers.authorization.split(' ')[1]).toString('base64') != encodedKey){
-        return res.json({"error": "Not Authorized"});
-     }
-    fs.readFile(`./scp/scp.json`, (err, data) => {
-        if (err) throw err;
-        let result = JSON.parse(data);
-        let random = Math.floor(Math.random() * (result.length)) + 1
-        if(random < 100){
-            if(random < 10){
-                random = "00"+random;
-            }
-            else{
-                random = "0"+random;
-            }
-            
+                 
+        const data = await pool.query('SELECT item_number, object_class, series, name, description, link FROM SCP WHERE item_number = $1', ['SCP-'+req.params.number])
+        let result = data.rows;
+        if(data.rows.length > 0){
+            return res.json(result);
         }
-        let filter = result.find(item => item.title === `SCP-${random}`)
-        res.json(filter);
-    });
+        else{
+            return res.status(404).json({"error": 'SCP was not found. Please check that you have put a correct query.'});
+        }
+        
+    
+}
+
+const getRandomSCP = async (req, res) => {
+    if(!req.headers.authorization){
+        return res.json({"error": "Not Authorized"});
+     }
+     else if(Buffer.from(req.headers.authorization.split(' ')[1]).toString('base64') != encodedKey){
+        return res.json({"error": "Not Authorized"});
+     }
+        
+        const data = await pool.query('SELECT item_number, object_class, series, name, description, link FROM SCP ORDER BY RANDOM() LIMIT 1');
+        const result = data.rows;
+        
+        res.json(result);
+    
+}
+
+const get404 = (req, res) => {
+    return res.status(404).json({"error": 'Resource was not found. Please check that you have put a correct query.'});
 }
 
 module.exports = {
     getAllSCPs,
     getSCP,
-    getRandomSCP
+    getRandomSCP,
+    get404
 }
