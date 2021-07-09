a85f57910692a47c20181cdb4b2d2b7db4aa2b8e
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-07-09 17:50:17 -0400

    Cambio a SSH

--- controllers/scp.controller.js
+++ controllers/scp.controller.js
@@ -9,8 +9,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: true
+    port: process.env.DB_PORT
   });
 
 const getAllSCPs =  async (req, res) => {
